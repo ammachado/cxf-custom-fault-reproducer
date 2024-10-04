0ea609b99d9db7215444caa07ac9f01689876c8b
Sundar Munirathnam Rajendran <smunirat@smunirat-mac.lan>
2024-10-04 15:58:20 -0500

    fixed the response body issue

--- src/test/java/my/example/customfault/utils/SoapRawClientResponse.java
+++ src/test/java/my/example/customfault/utils/SoapRawClientResponse.java
@@ -1,10 +1,17 @@
 package my.example.customfault.utils;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.io.StringWriter;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
+import lombok.Getter;
+import lombok.Setter;
 import my.example.customfault.common.InternalBusinessException;
 import my.example.customfault.common.XmlUtils;
 
@@ -16,6 +23,7 @@ public class SoapRawClientResponse {
     private Document httpResponseBody;
 
     public Node getElementByNameWithNamespace(String namespaceUrl, String elementName) {
+    	
         return httpResponseBody.getElementsByTagNameNS(namespaceUrl, elementName).item(0);
     }
 
@@ -26,8 +34,10 @@ public class SoapRawClientResponse {
 
     public String getBodyStringValue() {
         Node body = getElementByNameWithNamespace("http://schemas.xmlsoap.org/soap/envelope/", "Body");
-        Node node = body.getChildNodes().item(1);
-        return node != null ? node.getTextContent() : null;
+       
+        Node node = body.getChildNodes().item(0);
+       
+        return node != null ? nodeAsXml(node) : null;
     }
 
     public String getFaultStringValue() {
@@ -39,5 +49,18 @@ public class SoapRawClientResponse {
 
     public <T> T getUnmarshalledObjectFromSoapMessage(Class<T> jaxbClass) throws InternalBusinessException {
         return XmlUtils.getUnmarshalledObjectFromSoapMessage(httpResponseBody, jaxbClass);
+    }
+    
+    public String nodeAsXml(Node node) {
+    	String s = null;
+    	try {
+    	  StringWriter writer = new StringWriter();
+          Transformer transformer = TransformerFactory.newInstance().newTransformer();
+          transformer.transform(new DOMSource(node), new StreamResult(writer));
+           s = writer.toString();
+    	}catch(Exception e) {
+    		
+    	}
+    	return s;
     }
 }
