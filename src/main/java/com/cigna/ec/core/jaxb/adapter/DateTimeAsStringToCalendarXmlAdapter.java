22946fd5264d945e72cc312c58001226f5767697
smunirat <smunirat@smunirat-mac.lan>
2024-10-17 16:45:42 -0500

    adapters

---
+++ src/main/java/com/cigna/ec/core/jaxb/adapter/DateTimeAsStringToCalendarXmlAdapter.java
@@ -0,0 +1,22 @@
+package com.cigna.ec.core.jaxb.adapter;
+
+import java.util.Calendar;
+
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
+
+public class DateTimeAsStringToCalendarXmlAdapter extends XmlAdapter<String, Calendar>{
+
+	@Override
+	public Calendar unmarshal(String v) throws Exception {
+		return jakarta.xml.bind.DatatypeConverter.parseDateTime(v);
+	}
+
+	@Override
+	public String marshal(Calendar v) throws Exception {
+		if(v == null) {
+			return null;
+		}
+		return jakarta.xml.bind.DatatypeConverter.printDateTime(v);
+	}
+
+}
